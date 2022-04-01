fa0c505087ef34652f094004b640a1275a6c934e
nikihdyt <nikihidayati20@gmail.com>
2022-04-01 11:23:22 +0700

    memberitahu RecyclerView utk menggunakan Adapter dan mengubah MainAvtivity untuk menggunakan RecyclerView

--- app/src/main/java/com/example/android/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/android/affirmations/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.android.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.affirmations.adapter.ItemAdapter
+import com.example.android.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // inisialisasi data
+        val myDataSet = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+        // Setelan dibawah ini hanya diperlukan untuk meningkatkan performa.
+        // Gunakan setelan ini jika Anda mengetahui bahwa perubahan dalam content tidak mengubah ukuran dari layout RecyclerView.
+        recyclerView.setHasFixedSize(true)
     }
 }
